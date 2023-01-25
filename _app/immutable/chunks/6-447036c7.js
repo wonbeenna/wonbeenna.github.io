a7024f4c9efb7ddece41afe2ab0c5a6328b13130
wonbeenna <nwbnwb@naver.com>
2023-01-25 23:19:29 +0900

    Updates

---
+++ _app/immutable/chunks/6-447036c7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/blog/board/_page.svelte-937e93c9.js";const e=!0;export{t as component,e as has_server_load};
--- _app/immutable/chunks/9-697b03dc.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/blog/page/_page_/_page.svelte-7b3b5f2d.js";const e=!0;export{t as component,e as has_server_load};
