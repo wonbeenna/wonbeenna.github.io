88944b453975ef8d7f6b9f0305218a9c6f5b43e3
wonbeenna <nwbnwb@naver.com>
2023-01-26 15:42:01 +0900

    feature: 다크모드 기본

--- src/lib/components/Layout/Header.svelte
+++ src/lib/components/Layout/Header.svelte
@@ -1,9 +1,21 @@
 <script>
     import '$lib/styles/_layout.scss'
     import {base} from "$app/paths";
+    import { onMount } from 'svelte';
+
+    let currentTheme;
+
+    const setTheme = (theme) => {
+       currentTheme = theme;
+      document.documentElement.dataset.theme = theme;
+      document.cookie = `siteTheme=${theme};max-age=31536000;path='/'`;
+    }
+
+    onMount(() => {
+       currentTheme = document.documentElement.dataset.theme;
+    });
 </script>
 
-<!-- TODO 다크모드-->
 <header class="header">
    <section class="header_section">
       <div class="header_box">
@@ -23,19 +35,26 @@
       </div>
       <div class="header_box">
          <a href={'https://github.com/wonbeenna'} target="_blank" rel="noopener noreferrer">
-            <img src={`${base}/icons/github.svg`}
+            <img src={currentTheme === 'dark' ? `${base}/icons/github-light.svg` : `${base}/icons/github.svg`}
                  alt="github"
                  width={30}
                  height={30}
             />
          </a>
          <a href={'mailto:nwbnwb@naver.com'}>
-            <img src={`${base}/icons/mail.svg`}
+            <img src={currentTheme === 'dark' ? `${base}/icons/mail-light.svg` : `${base}/icons/mail.svg`}
                  alt="email"
                  width={34}
                  height={34}
             />
          </a>
+         <button  on:click={() => setTheme(currentTheme === 'dark' ? "light" : 'dark')}>
+            {#if currentTheme === 'dark'}
+               <img src="{`${base}/icons/sun.svg`}" alt="밝은 테마" width={30} height={30} />
+            {:else}
+               <img src="{`${base}/icons/moon.svg`}" alt="어두운 테마" width={30} height={30} />
+            {/if}
+         </button>
       </div>
    </section>
 </header>
