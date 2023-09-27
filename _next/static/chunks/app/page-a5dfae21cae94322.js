186cd42f67aa278add4b53877ac06b6073307c33
wonbeenna <nwbnwb@naver.com>
2023-09-27 17:50:43 +0900

    deploy to gh-pages

---
+++ _next/static/chunks/app/page-a5dfae21cae94322.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,254],{7830:function(e,n,s){Promise.resolve().then(s.t.bind(s,6964,23)),Promise.resolve().then(s.t.bind(s,4724,23))}},function(e){e.O(0,[964,724,971,864,744],function(){return e(e.s=7830)}),_N_E=e.O()}]);
