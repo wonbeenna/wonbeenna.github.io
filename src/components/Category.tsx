fc5b7c815f3b5edd1c87f1d422afa8bef1916494
nawonbeen <been@aptner.com>
2025-03-18 18:02:50 +0900

    feat: study-remind

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -13,14 +13,16 @@ const Category = ({ categories, currentCategory }: CategoryProps) => {
   return (
     <ul className="relative top-0 flex h-fit min-w-fit flex-row flex-wrap gap-[20px] py-[20px] pl-0 md:sticky md:top-[100px] md:flex-col md:flex-nowrap md:py-0 md:pl-[30px]">
       <h2 className="hidden text-[18px] font-bold md:inline">Category</h2>
-      {categories?.map((category) => (
-        <li key={category.title}>
-          <Link
-            className={`${category.title === currentCategory ? 'font-[600] text-black' : 'text-gray700'}`}
-            href={`/blog/${category.title}`}
-          >{`${category.title} (${category.count})`}</Link>
-        </li>
-      ))}
+      {categories
+        ?.toSorted((a, b) => b.count - a.count)
+        .map((category) => (
+          <li key={category.title}>
+            <Link
+              className={`${category.title === currentCategory ? 'font-[600] text-black' : 'text-gray700'}`}
+              href={`/blog/${category.title}`}
+            >{`${category.title} (${category.count})`}</Link>
+          </li>
+        ))}
     </ul>
   );
 };
