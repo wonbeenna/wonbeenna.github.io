163d9050a39e699bb2d732f830a908112b9e9112
nawonbeen <been@aptner.com>
2025-11-11 10:23:54 +0900

    fix: theme storageKey 수정

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -4,5 +4,9 @@ import React from 'react';
 import { ThemeProvider as Provider } from 'next-themes';
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return <Provider attribute="class">{children}</Provider>;
+  return (
+    <Provider attribute="class" storageKey={`${process.env.NEXT_PUBLIC_BASE_URL}-theme`}>
+      {children}
+    </Provider>
+  );
 }
